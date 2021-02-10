783501c7109554f198d43af99e7001287929e232
Meg <meganriley806@gmail.com>
2021-02-10 09:19:45 -0600

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -19,3 +19,6 @@ class Parser:
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / relative_to(source))
