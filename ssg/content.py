1cb53c7cac8e5618fd7f968b6dc4993d323bbad4
Meg <meganriley806@gmail.com>
2021-02-12 21:00:10 -0600

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -41,8 +41,7 @@ class Content(Mapping):
 
     def __repr__(self):
         data = {}
-        return str(data)
-
-        for key in self.data.items(key, value):
+        for key, value in self.data.items():
             if key != "content":
-                value = data[key]
+                data[key] = value
+        return str(data)
